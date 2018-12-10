64dd521828d68d684c0ad8d895791b6c2a350d43
Poensin <45597317+Poensin@users.noreply.github.com>
2018-12-10 15:01:20 +0100

    Add files via upload

--- Case.java
+++ Case.java
@@ -5,16 +5,53 @@
  */
 package morpion;
 
+
 /**
  *
  * @author poensing
  */
 public class Case {
     
+    private EtatCase etatCase;
     private int numCase;
     
-    Case(){
-        
+    
+    
+    Case(int numCase, EtatCase etatCase){
+        this.numCase = numCase;
+        this. etatCase = etatCase;
     }
+
+        
+        
+    
+    /**
+     * @return the etatCase
+     */
+    public EtatCase getEtatCase() {
+        return etatCase;
+    }
+
+    /**
+     * @param etatCase the etatCase to set
+     */
+    public void setEtatCase(EtatCase etatCase) {
+        this.etatCase = etatCase;
+    }
+
+    /**
+     * @return the numCase
+     */
+    public int getNumCase() {
+        return numCase;
+    }
+
+    /**
+     * @param numCase the numCase to set
+     */
+    public void setNumCase(int numCase) {
+        this.numCase = numCase;
+    }
+    
     
 }
--- Morpion.java
+++ Morpion.java
@@ -5,18 +5,51 @@
  */
 package morpion;
 
+import java.util.ArrayList;
+
+
+
+
 /**
  *
  * @author poensing
  */
 public class Morpion {
+    
+    
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Controleur c = new Controleur();
+        // TODO code application logic here
+        
+        ArrayList<Case> casesCochées = new ArrayList<>();
+        ArrayList<Case> cases = new ArrayList<>();
+        
+        
+        Case c1 = new Case(1,EtatCase.NON_COCHEE);
+        cases.add(c1);
+        Case c2 = new Case(2,EtatCase.NON_COCHEE);
+        cases.add(c2);
+        Case c3 = new Case(3,EtatCase.NON_COCHEE);
+        cases.add(c3);
+        Case c4 = new Case(4,EtatCase.NON_COCHEE);
+        cases.add(c4);
+        Case c5 = new Case(5,EtatCase.NON_COCHEE);
+        cases.add(c5);
+        Case c6 = new Case(6,EtatCase.NON_COCHEE);
+        cases.add(c6);
+        Case c7 = new Case(7,EtatCase.NON_COCHEE);
+        cases.add(c7);
+        Case c8 = new Case(8,EtatCase.NON_COCHEE);
+        cases.add(c8);
+        Case c9 = new Case(9,EtatCase.NON_COCHEE);
+        cases.add(c9);
+        
         
     }
+    
+
     
 }
